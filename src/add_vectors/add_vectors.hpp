a48ce74b73db6b8701a3f8cf4c8bb3098f086acc
Andrei Klimovich <andrey.klimovich@mapbox.com>
2018-07-25 00:12:21 +0300

    add vector addition kernel

---
+++ src/add_vectors/add_vectors.hpp
@@ -0,0 +1,6 @@
+
+void VectorFill(float * v, int size);
+
+void VectorAdd(float const * fst, float const * snd, float * res, int size, bool use_gpu=false);
+
+void VectorPrint(char const * name, float const * v, int size);
